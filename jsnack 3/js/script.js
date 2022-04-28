be99978df54ae978e90ea1e12a4e46f81decf673
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-28 15:29:41 +0200

    terzo esercizio terminato

--- jsnack 3/js/script.js
+++ jsnack 3/js/script.js
@@ -1,3 +1,23 @@
 /*
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 */
+
+let saluto = prompt('inserire una stringa da invertire');
+while(!isNaN(saluto)){
+  if(!isNaN(saluto)){
+    saluto = prompt('non hai inserito una stringa, riprovare')
+  }
+}
+
+const stringaInvertita = invertiStringhe(saluto);
+
+function invertiStringhe(stringa) {
+  const stringaInvertita = stringa.split('').reverse().join('');
+  console.log(stringaInvertita);
+  return stringaInvertita
+}
+
+document.getElementById('parola-invertita').innerHTML = 
+`
+La stringa inserita (${saluto}) invertita &egrave; "${stringaInvertita}".
+`;
